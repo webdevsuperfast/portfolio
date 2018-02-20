0c9ae6d1befad26994cc66c8283f1eb0f45b0cb4
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-02-20 18:06:00 +0800

    Gulp support

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-clean-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    prettify = require('gulp-jsbeautifier'),
+    vinylpaths = require('vinyl-paths'),
+    cmq = require('gulp-combine-mq'),
+    merge = require('merge-stream'),
+    foreach = require('gulp-flatmap'),
+    changed = require('gulp-changed'),
+    runSequence = require('run-sequence'),
+    browserSync = require('browser-sync'),
+    cp = require('child_process'),
+    del = require('del'),
+    jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+
+// Jekyll
+gulp.task( 'jekyll-build', function(done) {
+    browserSync.notify({message: 'Running jekyll build'});
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+} );
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', ['styles', 'scripts', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+// CSS
+gulp.task('styles', function(){
+    var sassStream = gulp.src('assets/scss/style.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(concat('app.scss'))
+    
+    var mergeStream = merge(sassStream)
+        .pipe(concat('app.css'))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(cmq())
+        .pipe(gulp.dest('temp/css'))
+        .pipe(rename('app.css'))
+        .pipe(prettify())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(notify({ message: 'Styles task complete' }));
+    
+    return mergeStream;
+});
+
+// JSHint
+gulp.task('lint', function(){
+    return gulp.src('assets/js/source/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+});
+
+// Scripts
+gulp.task('scripts', function() {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.slim.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/now-ui-kit/assets/js/now-ui-kit.js'
+    ])
+    .pipe(changed('js'))
+    .pipe(foreach(function(stream, file){
+        return stream
+            .pipe(uglify())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest('temp/js'))
+    }))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Clean
+gulp.task('clean', function(cb) {
+    return gulp.src('temp/*')
+    .pipe(vinylpaths(del))
+});
+
+// Default task
+gulp.task('default', ['browser-sync', 'clean', 'styles', 'lint', 'scripts', 'watch']);
+
+// Watch
+gulp.task('watch', function() {
+    // Watch .scss files
+    gulp.watch(['assets/scss/*.scss', 'assets/scss/**/*.scss'], ['styles']);
+
+    // Watch .js files
+    gulp.watch(['assets/js/vendor/*.js', 'assets/js/source/*.js'], ['scripts']);
+
+    // Watch .html files
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});

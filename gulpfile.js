93ebcfae9c5630972d06bd700c16eee304d8b0fe
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-02-22 12:37:20 +0800

    Modify jekyll-build task

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var gulp = require('gulp'),
 // Jekyll
 gulp.task( 'jekyll-build', function(done) {
     browserSync.notify({message: 'Running jekyll build'});
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+    return cp.spawn( jekyll , ['build', '--watch', '--incremental'], {stdio: 'inherit'})
         .on('close', done);
 } );
 

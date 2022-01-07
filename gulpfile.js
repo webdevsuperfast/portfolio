bdc6a9d5953bfdc6887410b6729dd299e3214703
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-01-07 11:58:44 +0800

    Cleaned up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
-const { src } = require('gulp');
-const tailwindcss = require('tailwindcss');
+const { src, dest, task, watch, series, parallel } = require('gulp');
 
-const gulp = require('gulp'),
-    options = require('./config.js'),
+const options = require('./config.js'),
     sass = require('gulp-sass')(require('sass')),
     postcss = require('gulp-postcss'),
     uglify = require('gulp-uglify'),
@@ -12,7 +10,7 @@ const gulp = require('gulp'),
     foreach = require('gulp-flatmap'),
     browserSync = require('browser-sync').create(),
     cp = require('child_process'),
-    autoprefixer = require('autoprefixer'),
+    // autoprefixer = require('autoprefixer'),
     cssnano = require('cssnano'),
     changed = require('gulp-changed'),
     critical = require('critical'),
@@ -20,73 +18,21 @@ const gulp = require('gulp'),
     webp = require('gulp-webp'),
     jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 
-var plugins = [
-    tailwindcss(options.config.tailwind),
-    autoprefixer,
-    cssnano({
-        preset: ['default', {
-            discardComments: {
-                removeAll: true,
-            },
-        }]
-    })
-]
-
-var styles,
-    scripts,
-    images,
-    sources;
-
-styles = {}
-scripts = {}
-images = {}
-sources = {}
-
-styles.src = 'assets/scss/style.scss';
-styles.dest = 'assets/css';
-
-scripts.src = [
-    'node_modules/stream/assets/vendors/bootstrap/js/bootstrap.js',
-    'node_modules/stream/assets/js/global.js',
-    'node_modules/stream/assets/vendors/jquery.parallax.js',
-    'node_modules/stream/assets/js/vendors/parallax.js',
-    'node_modules/mixitup/dist/mixitup.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-    'node_modules/vanilla-lazyload/dist/lazyload.js'
-];
-scripts.minified = [
-    'node_modules/stream/assets/vendors/popper.min.js',
-    'node_modules/stream/assets/vendors/jquery.min.js',
-    'node_modules/stream/assets/vendors/jquery.migrate.min.js',
-    'node_modules/stream/assets/vendors/jquery.back-to-top.min.js'
-];
-scripts.dest = 'assets/js';
-
-images.feature = [
-    './develop/images/*.{jpg,png}',
-    '!./develop/images/school-for-selling.jpg',
-    '!./develop/images/*.svg',
-    '!./develop/images/thumbs/*.{jpg,png}'
-];
-images.thumbnail = [
-    './develop/images/thumbs/*'
-];
-
-sources.src = 'node_modules/stream/assets/include/scss/**/**';
-sources.dest = 'assets/scss/sources/';
-
 function jekyllBuild() {
     return cp.spawn( jekyll, ['build'], { stdio: 'inherit' });
 }
 
 function style() {
-    return gulp.src(options.paths.styles.src)
-        .pipe(changed(options.paths.styles.dest))
+    const tailwindcss = require('tailwindcss');
+    return src(`${options.paths.styles.src}/*.scss`)
         .pipe(sass().on('error', sass.logError))
-        .pipe(concat('app.scss'))
-        .pipe(postcss(plugins))
+        .pipe(concat('style.scss'))
+        .pipe(postcss([
+            tailwindcss(options.config.tailwind),
+            require('autoprefixer')
+        ]))
         .pipe(rename('app.css'))
-        .pipe(gulp.dest(options.paths.styles.dest))
+        .pipe(dest(options.paths.styles.dest))
         .pipe(browserSync.reload({ stream: true }))
         .pipe(notify({ 
             'message': 'Styles task complete' 
@@ -108,13 +54,13 @@ function criticalCss() {
 }
 
 function js() {
-    return gulp.src(options.paths.scripts.src)
+    return src(options.paths.scripts.src)
     .pipe(foreach(function(stream, file){
         return stream
             .pipe(uglify())
             .pipe(rename({suffix: '.min'}))
     }))
-    .pipe(gulp.dest(options.paths.scripts.dest))
+    .pipe(dest(options.paths.scripts.dest))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ 
         'message': 'Scripts task complete' 
@@ -135,9 +81,9 @@ function browserSyncReload(done) {
     done();
 }
 
-function watch() {
-    gulp.watch(['assets/scss/style.scss', 'assets/scss/**/**/*.scss'], style);
-    gulp.watch(
+function watchFiles() {
+    watch(['assets/scss/style.scss', 'assets/scss/**/**/*.scss'], style);
+    watch(
     [
         '*.html', 
         '_layouts/*.html', 
@@ -149,7 +95,7 @@ function watch() {
         'assets/js/*.js',
         'assets/images/*.*'
     ],
-    gulp.series(jekyllBuild, browserSyncReload));
+    series(jekyllBuild, browserSyncReload));
 }
 
-gulp.task('default', gulp.parallel(jekyllBuild, style, criticalCss, gulp.series(js), browserSyncServe, watch));
+task('default', parallel(jekyllBuild, style, criticalCss, series(js), browserSyncServe, watchFiles));

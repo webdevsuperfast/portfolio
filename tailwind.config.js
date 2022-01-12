5808717a1df60281022cbe38c72dc3d37f0e94a2
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-01-12 20:25:33 +0800

    Added additional config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
   content: [
     './index.html',
+    './**/index.html',
     './_layouts/**/*.html',
     './_includes/**/*.html',
     './_site/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        sacramento: ['Sacramento', 'cursive']
+      },
+      fontSize: {
+        '14xl': ['14rem', {
+          lineHeight: '1'
+        }]
+      }
+    },
   },
   plugins: [],
 }
